a3abf5a5b30b8bfcb2a3d16a33e5c513f1463355
Oli <oliverrsaxon@gmail.com>
2021-03-02 21:37:51 +0000

    FEATURE: Option to include/exclude certain character types from the generated password.

--- assets/script.js
+++ assets/script.js
@@ -8,21 +8,36 @@ const charsLower = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o',
 const charsUpper = charsLower.map(i => i.toUpperCase());
 const specChars = ['_','!','#','£','-','$'];
 const numbers = ['0','1','2','3','4','5','6','7','8','9'];
+var string = "";
 
 function generatePassword(){
-    var string = "";
+    var string = ""
     var finalPW = "";
-    var pwLength = 12 //window.prompt("Password length");
-    for (i = 0; i < pwLength; i++){
-        var lower = getRandom(charsLower.length);
-        var upper = getRandom(charsUpper.length);
-        var spec = getRandom(specChars.length);
-        var num = getRandom(numbers.length);
-        string += charsLower[lower];
-        string += charsUpper[upper];
-        string += specChars[spec];
-        string += numbers[num];
+    var pwLength = window.prompt("Please specify the length of the password (between 8 and 128");
+    var optionUpperAndLower = window.confirm("Click confirm to include upper and lower case characters (cancel for lower only");
+    var optionSpecChars = window.confirm("Click confirm to include special characters");
+    var optionNumbers = window.confirm("Click confirm to include numbers");
+    if (optionUpperAndLower){
     }
+
+    for (i = 0; i < pwLength; i++){
+        if (optionUpperAndLower){
+            string += charsUpper[getRandom(charsUpper.length)];
+            string += charsLower[getRandom(charsLower.length)];
+        } else {
+            string += charsLower[getRandom(charsLower.length)];
+        }
+        if (optionSpecChars){
+            string += specChars[getRandom(specChars.length)];
+        }
+        if (optionNumbers){
+            string += getRandom(9);
+        }
+        
+    }
+    console.log("Upper and lower option ::", optionUpperAndLower);
+    console.log("Special chars option ::", optionSpecChars);
+    console.log("Numbers option ::", optionNumbers);
 
     for (i = 0; i < pwLength; i++){
         finalPW += string[getRandom(string.length)];
@@ -30,6 +45,7 @@ function generatePassword(){
     console.log(finalPW);
     passwordBox.innerHTML = finalPW;
 }
+
 
 
 var generateButton = document.getElementById("generate");
