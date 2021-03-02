043d0262ae238082d3f115f7ac657f2e9a102d99
Oli <oliverrsaxon@gmail.com>
2021-03-02 14:56:26 +0000

    SCRIPT: Function implemented which generates a password of a given length comprising of lower case, upper case and special characters

--- assets/script.js
+++ assets/script.js
@@ -2,18 +2,27 @@ function getRandom(max){
     return Math.floor(Math.random() * Math.floor(max));
 }
 
-const charsLower = ['a','b','c','d'];
+const charsLower = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 const charsUpper = charsLower.map(i => i.toUpperCase());
 const specChars = ['_','!','#','£','-','$'];
 
 function generatePassword(){
-    var password = "";
+    var string = "";
+    var finalPW = "";
     var pwLength = 12 //window.prompt("Password length");
     for (i = 0; i < pwLength; i++){
-        var char = getRandom(charsLower.length)
-        password += charsLower[char];
+        var char1 = getRandom(charsLower.length)
+        var char2 = getRandom(charsUpper.length)
+        var char3 = getRandom(specChars.length)
+        string += charsLower[char1];
+        string += charsUpper[char2];
+        string += specChars[char3];
     }
-    console.log(password)
+
+    for (i = 0; i < pwLength; i++){
+        finalPW += string[getRandom(string.length)];
+    }
+    console.log(finalPW);
 }
 
 
