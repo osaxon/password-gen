182daf331707eebbdc609244fed8c37726d90eb2
Oli <oliverrsaxon@gmail.com>
2021-03-02 14:03:42 +0000

    SCRIPT: created a function to generate a random number which will be used to select a character from an array in a for loop

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,21 @@
-function logMsg(){
-    console.log("logged")
+function getRandom(max){
+    return Math.floor(Math.random() * Math.floor(max));
 }
 
 const charsLower = ['a','b','c','d'];
 const charsUpper = charsLower.map(i => i.toUpperCase());
-console.log(charsUpper, charsLower);
+const specChars = ['_','!','#','£','-','$'];
+
+
+function generatePassword(){
+    let password = "";
+    let pwLength = 8 //window.prompt("Password length");
+    for (i = 0; i < pwLength; i++){
+    }
+    console.log(charsLower[getRandom(charsLower.length)]);
+}
+
 
 var generateButton = document.getElementById("generate");
 
-generateButton.addEventListener("click", logMsg)
+generateButton.addEventListener("click", generatePassword)
